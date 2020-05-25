3ae5ba8cf671de5cfdc9a52a55f6bc85a7b181bf
Andreas J Mueller <amueller6@cnm.edu>
2020-05-25 06:12:51 +0000

    module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false
+    };
+  }
+
   handleClick(buttonText) {
-    if (buttonText === this.props.quiz_question.answer) {
+    const correctAnswer = buttonText === this.props.quiz_question.answer;
+    if (correctAnswer) {
       this.props.showNextQuestionHandler();
     }
+    this.setState({ incorrectAnswer : !correctAnswer });
   }
 
   render() {
@@ -23,6 +32,8 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
+        {this.state.incorrectAnswer ?
+          <p className="error">Sorry, that's not right</p> : null}
       </main>
     );
   }
